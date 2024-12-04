9d2472f30dd4dc6996c77adc4725f8759c92e428
Chriss-54 <121314148+Chriss-54@users.noreply.github.com>
2024-12-04 10:48:26 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,21 +1,56 @@
 #include <iostream>
-#include "Frac.h" // Incluye el archivo de encabezado, no el de implementación
-
+#include <string>
 using namespace std;
+int main(){
+    float estatura;
+    string nombre, lugarFavorito, colorFavorito;
+    char inicialApellido, simboloRepresentativo;
+    int numeroSuerte, añoNacimiento;
+    short hermanos;
+    double numeroMagico;
 
-int main() {
-    Frac F1, F2, result; // Corrige la declaración de F2 y result
+    cout<<"=== GENERADOR DE DNI CURIOSO ==="<<endl;
+    cout<<"Ingrese su nombre: ";
+    getline(cin,nombre);
 
-    system("cls"); // Limpia la pantalla
+    cout<<"Ingrese la inicial de su apellido: ";
+    cin>>inicialApellido;
+    cin.ignore();
 
-    cout << "Ingrese la primera fracción: " << endl;
-    F1.in();
+    cout<<"Ingrese su estatura (m): ";
+    cin>>estatura;
 
-    cout << "Ingrese la segunda fracción: " << endl;
-    F2.in();
+    cout<<"Ingrese su numero magico favorito: ";
+    cin>>numeroMagico;
 
-    result = F1 * F2; // Realiza la operación de multiplicación
-    result.out();
+    cout<<"Ingrese su lugar favorito: ";
+    getline(cin,lugarFavorito);
+
+    cout<<"Ingrese numero de hermanos:";
+    cin>>hermanos;
+
+    cout<<"Ingrese su año de nacimiento:";
+    cin>>añoNacimiento;
+
+    cout<<"Ingrese su numero de la suerte: ";
+    cin>>numeroSuerte;
+    cin.ignore();
+
+    cout<<"Ingrese su color favorito:";
+    getline(cin, colorFavorito);
+
+    int edad = 2024 - añoNacimiento;
+    long codigoPersonal = static_cast<long>(numeroSuerte * numeroMagico);
+
+    cout<<"\n=== DOCUMENTO DE IDENTIDAD CURIOSO ==="<<endl;
+    cout<<"Nombre Completo:" <<nombre<<""<<inicialApellido<<"."<<endl;
+    cout<<"Simbolo Personal:"<<simboloRepresentativo<<endl;
+    cout<<"edad:"<<edad<<"años"<<endl;
+    cout<<"Codigo personal:" <<simboloRepresentativo<<endl;
+    cout<<"Lugar favorito: "<<lugarFavorito<<endl;
+    cout<<"Color Favorito: "<<colorFavorito<<endl;
+    cout<<"Estatura: "<<estatura<<"m"<<endl;
+    cout<<"Numero Hermanos: "<<hermanos<<endl;
 
     return 0;
 }
